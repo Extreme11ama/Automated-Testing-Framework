d1ddc15a7ab11b41802e94057766f0fd37c43153
Extreme11ama <wasifram@gmail.com>
2026-03-06 16:23:59 -0800

    added context

--- conftest.py
+++ conftest.py
@@ -5,6 +5,8 @@ from playwright.sync_api import sync_playwright
 def page():
     with sync_playwright() as p:
         browser = p.chromium.launch()
+        context = browser.new_context()
         page = context.new_page()
         yield page
         browser.close
+        
